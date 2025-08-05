f70fc324c467d889eaf41182ab2aa84748d681b5
Thesly Silva <thesley456silva@gmail.com>
2025-08-05 18:38:26 -0300

    Create Home.java

---
+++ src/main/java/com/thesly/projeto/controllers/Home.java
@@ -0,0 +1,13 @@
+package com.thesly.projeto.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @GetMapping
+    public String home(){
+        return "Hola mundo";
+    }
+}
